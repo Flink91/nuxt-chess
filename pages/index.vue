ccd9188bf6b5e1b004c90ed81c3c22dd9cb84364
Felix Gellner <flink@flinkblog.de>
2022-12-06 01:26:20 +0100

    testing vue-chess, imported as nuxt plugin

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <Tutorial/>
+  <FlexibleChessboard />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
 })
 </script>
---
+++ plugins/chessboard.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import chessboard from 'vue-chessboard'
+import 'vue-chessboard/dist/vue-chessboard.css'
+
+Vue.use(chessboard)
